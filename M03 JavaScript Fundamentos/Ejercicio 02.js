9bbefa9db975f4ad59526628d290a3fcbe11b8ea
TheForcesOfNature <juancarlosalderetears@gmail.com>
2023-09-15 22:23:42 -0300

    completed

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -2,43 +2,55 @@
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
 function devolverString(string) {
-   // Debe retornar un string.
-   // Tu código:
+  // Debe retornar un string.
+  // Tu código:
+  return string;
 }
+console.log(devolverString());
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
-   // Retorna el resultado de su suma.
-   // Tu código:
+  // Retorna el resultado de su suma.
+  // Tu código:
+  return x + y;
 }
+console.log(suma(9, 2));
 
 function resta(x, y) {
-   // Retorna el resultado de la resta.
-   // Tu código:
+  // Retorna el resultado de la resta.
+  // Tu código:
+  return x - y;
 }
+console.log(resta(9, 2));
 
 function divide(x, y) {
-   // Retorna el resultado de su división.
-   // Tu código:
+  // Retorna el resultado de su división.
+  // Tu código:
+  return x / y;
 }
+console.log(divide(9, 2));
 
 function multiplica(x, y) {
-   // Retorna el resultado de su multiplicación.
-   // Tu código:
+  // Retorna el resultado de su multiplicación.
+  // Tu código:
+  return x * y;
 }
+console.log(multiplica(9, 2));
 
 function obtenerResto(x, y) {
-   // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+  // Obten el resto de la división de "x" entre "y".
+  // Tu código:
+  return x % y;
 }
+console.log(obtenerResto(9, 2));
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   devolverString,
-   suma,
-   resta,
-   divide,
-   multiplica,
-   obtenerResto,
+  devolverString,
+  suma,
+  resta,
+  divide,
+  multiplica,
+  obtenerResto,
 };
